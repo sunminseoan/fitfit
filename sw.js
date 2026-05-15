65137d0a3d059584000873fe9e031326a61719a5
sunminseoan <eeesunmin@gmail.com>
2026-05-15 15:32:08 +0900

    PWA 추가 - 모바일 앱처럼 설치 가능

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'fitfit-v1';
+const urlsToCache = ['/fitfit-app.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
